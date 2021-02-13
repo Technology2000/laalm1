8778fd73300dc8be37e7dc83504ad2632d0c7578
sravani <sravanik6800@gmail.com>
2021-02-13 08:51:48 +0530

    laalmproject

---
+++ lib/reports.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class reports extends StatefulWidget {
+  @override
+  _reportsState createState() => _reportsState();
+}
+
+class _reportsState extends State<reports> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Reports')),
+    );
+  }
+}
